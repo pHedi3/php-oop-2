d16da949f9c1e022f1091f37fb78370328837c1f
pHedi3 <federico.gualdi3@gmail.com>
2021-09-07 16:31:27 +0200

    rimane parte price

--- index.php
+++ index.php
@@ -6,17 +6,32 @@ class EShop {
 
     private $addres;
 
-    private $prodoctAvailable;
+    private $prodoctAvailable = [];
 
-    private $prodoctNotAvailable;
+    private $user = [];
 
-    private $user;
-
-    private $userPremium;
+    private $userPremium = [];
 
     public function __construct(string $name, string $adres) {
         $this->name = $name;
         $this->addres = $adres;
+    }
+
+    public function addProdoct(Product $prodoct) {
+        $this->prodoctAvailable[] = $prodoct;
+    }
+
+    public function addUser(User $var) {
+        $this->user[] = $var;
+    }
+    public function addUserPremium(PremiumUser $var) {
+        $this->userPremium[] = $var;
+    }
+
+    public function removeProdoct(Product $var) {
+        $id = array_search($var, $this->prodoctAvailable);
+        unset($this->prodoctAvailable[$id]);
+        $this->prodoctAvailable = array_values($this->prodoctAvailable);
     }
 
     
@@ -31,10 +46,13 @@ class Product {
     private $price;
     private $weight; 
 
-    public function __construct(string $label, string $name, string $price) {
+    public function __construct(string $label, string $name, int $price) {
         $this->label = $label;
         $this->name = $name;
         $this->price = $price;
+    }
+    public function getPrice() {
+        echo $this->price;
     }
 
 }
@@ -44,12 +62,6 @@ Class TechProduct extends Product {
     private $needBattery;
     private $compatibility;
     private $os;
-    public function __construct(string $label, string $name, int $price, bool $bat ) {
-        $this->label = $label;
-        $this->name = $name;
-        $this->price = $price;
-        $this->needBattery = $but;
-    }
 
 }
 
@@ -57,12 +69,6 @@ Class BeautyProduct extends Product {
 
     private $uses;
     private $composition;
-    public function __construct(string $label, string $name, int $price, string $use ) {
-        $this->label = $label;
-        $this->name = $name;
-        $this->price = $price;
-        $this->uses = $use;
-    }
 }
 
 // ---------------------------------------------
@@ -71,8 +77,23 @@ class User {
 
     private $nameUser;
     private $addres;
+    private $prodoctS = [];
     private $Card;
     protected $sconto = 0;
+
+    public function __construct(string $name, string $adres) {
+        $this->nameUser = $name;
+        $this->addres = $adres;
+    }
+
+    public function addCard(string $name, string $number, string $date) {
+        $this->card = new Card($name, $number, $date);
+    }
+
+    public function addProdoct(Product $var) {
+        $this->prodoctS[] = $var;
+    }
+
 
 }
 
@@ -80,29 +101,67 @@ class PremiumUser {
 
     private $nameUser;
     private $addres;
-    private $Card;
+    private $prodoctS = [];
+    private $card;
     protected $sconto = 50;
+    public function __construct(string $name, string $adres) {
+        $this->nameUser = $name;
+        $this->addres = $adres;
+    }
+
+    public function addCard(string $name, string $number, string $date) {
+        $this->card = new Card($name, $number, $date);
+    }
 
 
+}
+
+// --------------------------------------------
+class Card {
+    private $name;
+    private $number;
+    private $prodoctS = [];
+    private $expirationDate;
+    public function __construct(string $name, string $number, string $date) {
+        $this->name = $name;
+        $this->number = $number;
+        $this->expirationDate = $date;
+    }
 }
 
 // ----------------------------------------------
 
 $hooli = new EShop('Hooli', 'Via Parto in Tre');
-var_dump($hooli);
 
-$smartPhone = new TechProduct('iphone', 'iphon 12 pro', 1200, false);
-$toyCar = new TechProduct('toy Car Power', 'extrem Car', 240, true);
-$cream = new BeautyProduct('hand cream', 'levinel', 20, 'for hand and body');
-$shampo = new BeautyProduct('shampo capelli grassi', 'paludon', 3, 'for hair');
-$book = new Product('the dust', 'the triology of dust', 12)
+$smartPhone = new TechProduct('iphone', 'iphon 12 pro', 1200);
+$toyCar = new TechProduct('toy Car Power', 'extrem Car', 240);
+$cream = new BeautyProduct('hand cream', 'levinel', 20);
+$shampo = new BeautyProduct('shampo capelli grassi', 'paludon', 3);
+$book = new Product('the dust', 'the triology of dust', 12);
 
+$hooli->addProdoct($smartPhone);
+$hooli->addProdoct($toyCar);
+$hooli->addProdoct($cream);
+$hooli->addProdoct($shampo);
+$hooli->addProdoct($book);
+
+$gino = new User('Gino', 'Via Pollo Spennato');
+$gastone = new PremiumUser('Gastone', 'Via Riccanza');
+
+$hooli->addUser($gino);
+$hooli->addUserPremium($gastone);
+
+$gino->addCard('Gino', '20203030', '2020-08-9');
+$gastone->addCard('Gastone', '007', '2040-12-23');
+
+function buyProdoct($hooli, $who, $prodoct) {
+    $hooli->removeProdoct($prodoct);
+    $who->addProdoct($prodoct);
+    echo 'devi pagare ' . $prodoct->getPrice();
+}
+buyProdoct($hooli, $gino, $cream);
 
 /*
-3. aggiungiamoli all'eshop
-4. creaiamo l'utente normale
-5. creiamo un utente premium
-6. inseriamo le carte di credito per ciascun utente
 6. l'utente normale acquista un prodotto
 7. l'utente premium acquista un altro prodotto (e riceve lo sconto)
 */
